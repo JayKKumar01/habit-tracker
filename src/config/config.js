b3b76491d6e0d71182dc759e93c322dcfc289a2a
jaykkumar01 <jaytechbc@gmail.com>
2025-09-14 19:09:46 +0530

    Latest frontend updates

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,3 @@
 const BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:9090/api";
 
 export default BASE_URL;
-
-// Set the base path for GitHub Pages
-export const BASENAME =
-    process.env.NODE_ENV === "production" ? "/habit-tracker-frontend" : "/";
